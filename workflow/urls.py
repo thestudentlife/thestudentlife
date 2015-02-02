d7437c563e36bac1624239788bf76ce0428a2eb4
Ziqi Xiong <zx012013@mymail.pomona.edu>
2015-02-01 18:15:06 -0800

    create urls files)

--- workflow/urls.py
+++ workflow/urls.py
@@ -1,13 +1,9 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
+from workflow import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'website.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^workflow/', include('workflow.urls')),
-    url(r'^', include('mainsite.urls')),
 )
