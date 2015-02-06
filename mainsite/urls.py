2efbe9d6f238ad17b9de376f9d8ab8a0e6b8358a
theworldbright <sushionigiris@gmail.com>
2015-02-06 14:32:49 -0800

    Add URLs for main site

--- mainsite/urls.py
+++ mainsite/urls.py
@@ -2,8 +2,13 @@ from django.conf.urls import patterns, include, url
 from mainsite import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'website.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'views.home', name='home'),
+    url(r'^(?P<section>\w+)/$', 'views.section', name='section'),
+    url(r'^(?P<section>\w+)/(?P<article_id>\d+)/(?P<article>.+)/$', 'views.article', name='article'),
+    url(r'^author/(?P<author_id>\d+)/(?P<author_name>[\w]+(-[\w]+)/$', 'views.author', name='author'),
+    url(r'^photographer/(?P<photographer_id>\d+)/(?P<photographer_name>[\w]+(-[\w]+)/$', 'views.photographer', name='photographer'),
+    url(r'^staff$', 'views.staff', name='staff'),
+    url(r'^subscriptions', 'views.subscriptions', name='subscriptions'),
+    url(r'^about', 'views.about', name='about'),
+    url(r'^archives', 'views.archives', name='archives'),
 )
