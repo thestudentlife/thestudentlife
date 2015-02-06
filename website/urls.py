13d09e2ef9753f7dca01ba94ed7563298dfeff01
theworldbright <sushionigiris@gmail.com>
2015-02-06 15:03:04 -0800

    Remove unnecessary comments

--- website/urls.py
+++ website/urls.py
@@ -3,10 +3,6 @@ from django.contrib import admin
 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'website.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     url(r'^workflow/', include('workflow.urls')),
     url(r'^', include('mainsite.urls')),
