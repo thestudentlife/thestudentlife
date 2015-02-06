30283bba8507fb91cfa349137edcd4c15d817a1f
theworldbright <sushionigiris@gmail.com>
2015-02-06 15:02:29 -0800

    Add dummy view methods

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse('This is the homepage');
+
+def section(request):
+    return HttpResponse('This is a section page');
+
+def article(request, article_id):
+    return HttpResponse('This is article ' + str(article_id));
+
+def photographer(request, photographer_id):
+    return HttpResponse('This is photographer ' + str(photographer_id));
+
+def staff(request):
+    return HttpResponse('Staff page');
+
+def subscriptions(request):
+    return HttpResponse('Subscriptions page');
+
+def about(request):
+    return HttpResponse('About page');
+
+def archives(request):
+    return HttpResponse('Archives page');
