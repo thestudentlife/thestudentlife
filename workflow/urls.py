e829d279c35b9f8d72a970baa3733c8d02bd4fe1
theworldbright <sushionigiris@gmail.com>
2015-02-06 14:46:56 -0800

    Modify URLs for workflow

--- workflow/urls.py
+++ workflow/urls.py
@@ -12,18 +12,16 @@ urlpatterns = patterns('',
     url(r'^articles/issue/(?P<issue_id>[0-9]+)/$',views.issue,name="issue"),
     url(r'^articles/issue/new/$',views.new,name="new_issue"),
     #articles
-    url(r'^articles/issue/(?P<issue_id>[0-9]+)/(?P<article_id>[0-9]+)(,?P<article_name>.+)?/$',
+    url(r'^articles/issue/(?P<issue_id>[0-9]+)/(?P<article_id>[0-9]+)(/?P<article_name>.+)?/$',
     	views.article,name='article'),
     url(r'^articles/issue/(?P<issue_id>[0-9]+)/new/$',views.new_article,name='new_article'),
-    url(r'^articles/issue/(?P<issue_id>[0-9]+)/(?P<article_id>[0-9]+)(,?P<article_name>.+)?/edit/$',
+    url(r'^articles/issue/(?P<issue_id>[0-9]+)/(?P<article_id>[0-9]+)(/?P<article_name>.+)?/edit/$',
     	views.edit_article,name='edit_article'),
-    url(r'^articles/issue/(?P<issue_id>[0-9]+)/(?P<article_id>[0-9]+)(,?P<article_name>.+)?/delete/$',
+    url(r'^articles/issue/(?P<issue_id>[0-9]+)/(?P<article_id>[0-9]+)(/?P<article_name>.+)?/delete/$',
     	views.delete_article,name='delete_article'),
 	#photos
 	url(r'^photos/',views.photos,name="photos"),
 	url(r'^photos/(?P<photo_id>[0-9]+)/',views.photo,name="photo"),
 	url(r'^photos/new/',views.new_photo,name="new_photo"),
 	url(r'^photos/(?P<photo_id>[0-9]+)/edit/',views.edit_photo,name="edit_photo"),
-
-
 )
