9835e579a9a70fb3ead7f3a05eaf24f6c5103270
Ziqi Xiong <zx012013@mymail.pomona.edu>
2015-02-27 14:08:56 -0800

    finish initial data

--- workflow/migrations/initial_data.py
+++ workflow/migrations/initial_data.py
@@ -12,5 +12,29 @@ edit_type = ContentType.objects.get(app_label="auth", model="user")
 edit_perm=Permission(name="edit",content_type=edit_type,codename="edit")
 edit_perm.save()
 
+manage_type = ContentType.objects.get(app_label="auth", model="user")
+manage_perm=Permission(name="manage",content_type=edit_type,codename="manage")
+manage_perm.save()
+
+bronze=Group(name="bronze",permissions=[assignment_perm,post_perm])
+bronze.save();
+
+silver=Group(name="silver",permissions=[assignment_perm,post_perm,edit_perm])
+silver.save();
+
+gold=Group(name="gold",permissions=[assignment_perm,post_perm,edit_perm,manage_perm])
+gold.save();
+
+kent=User(username="kshikama")
+kent.save()
+kent.groups.add(gold)
+
+zq=User(username="zxiong")
+zq.save()
+zq.groups.add(gold)
+
+latina=User(username="vlatina")
+latina.save()
+latina.groups.add(gold)
 
 
