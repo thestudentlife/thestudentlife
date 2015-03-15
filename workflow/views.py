75797a3c339dac7aadca63237820d2937679ce30
theworldbright <sushionigiris@gmail.com>
2015-03-15 15:57:44 -0700

    Reformat workflow/views.py

--- workflow/views.py
+++ workflow/views.py
@@ -16,6 +16,7 @@ def group_required(*group_names):
             if bool(u.groups.filter(name__in=group_names)) | u.is_superuser:
                 return True
         return False
+
     return user_passes_test(in_groups, '/workflow/login')
 
 def register(request):
@@ -31,8 +32,8 @@ def register(request):
     else:
         registerForm = RegisterForm()
         return render(request, 'register.html', {
-        'form': registerForm
-     })
+            'form': registerForm
+        })
 
 def login(request):
     if request.method == "POST":
@@ -41,19 +42,19 @@ def login(request):
         password = request.POST['password']
         user = authenticate(username=username, password=password)
         if user:
-            do_login(request,user)
+            do_login(request, user)
             next = request.GET.get('next')
             if next is None:
                 return HttpResponse('Welcome')
             else:
                 return HttpResponseRedirect(request.GET['next'])
         else:
-            return render(request,'login.html',{
-            'form':loginForm
+            return render(request, 'login.html', {
+                'form': loginForm
             })
     else:
         loginForm = LoginForm()
-        return render(request,'login.html',{
+        return render(request, 'login.html', {
             'form': loginForm
         })
 
@@ -72,17 +73,17 @@ def issue(request, issue_id):
     issue = Issue.objects.get(pk=issue_id)
     sections = Section.objects.all()
     articles = Article.objects.filter(issue=issue)
-    return render(request,'issue.html',{'issue':issue,'sections':sections,'articles':articles})
+    return render(request, 'issue.html', {'issue': issue, 'sections': sections, 'articles': articles})
 
 @group_required('silver')
 def new_issue(request):
     return HttpResponse('Create a new issue')
 
-
-#articles
+# articles
 @group_required('silver')
 def article(request, issue_id, article_id, article_name="default"):
-    return HttpResponse('This is issue ' + str(issue_id) + " and article " + str(article_id) + ' with name ' + article_name)
+    return HttpResponse(
+        'This is issue ' + str(issue_id) + " and article " + str(article_id) + ' with name ' + article_name)
 
 @login_required(login_url='/workflow/login/')
 def new_article(request, issue_id):
@@ -97,10 +98,10 @@ def delete_article(request, issue_id, article_id, article_name="default"):
     return HttpResponse('You are going to delete article ' + str(article_id) + ' with name ' + article_name)
 
 @group_required('silver')
-def article_xml(request,article_id):
+def article_xml(request, article_id):
     article = Article.objects.get(id=article_id)
-    data = render_to_string('article_xml.xml',{'article':article})
-    return HttpResponse(data,content_type='application/xml')
+    data = render_to_string('article_xml.xml', {'article': article})
+    return HttpResponse(data, content_type='application/xml')
 
 #photos
 @group_required('silver')
@@ -123,31 +124,30 @@ def edit_photo(request, photo_id):
 @group_required('bronze')
 def assignments(request):
     assignments = Assignment.objects.all()
-    return render(request,'assignments.html',{'assignments':assignments})
+    return render(request, 'assignments.html', {'assignments': assignments})
 
 @group_required('bronze')
 def assignment(request, assignment_id):
     assignment = Assignment.objects.get(id=assignment_id)
-    return render(request,'assignment.html',{'assignment':assignment})
+    return render(request, 'assignment.html', {'assignment': assignment})
 
 @group_required('silver')
 def new_assignment(request):
     if request.method == 'GET':
         form = AssignmentForm()
-        return render(request,'new_assignment.html',{'form':form})
+        return render(request, 'new_assignment.html', {'form': form})
     else:
-         form = AssignmentForm(request.POST)
-         if form.is_valid():
-                new_assignment=form.save(commit=False)
-                new_assignment.sender=request.user.profile
-                new_assignment.save()
-                form.save_m2m
-                return HttpResponse('Thanks for assignment')
-         else:
-                return render(request, 'new_assignment.html', {
-                        'form': form
-                    })
-
+        form = AssignmentForm(request.POST)
+        if form.is_valid():
+            new_assignment = form.save(commit=False)
+            new_assignment.sender = request.user.profile
+            new_assignment.save()
+            form.save_m2m
+            return HttpResponse('Thanks for assignment')
+        else:
+            return render(request, 'new_assignment.html', {
+                'form': form
+            })
 
 @group_required('silver')
 def edit_assignment(request, assignment_id):
