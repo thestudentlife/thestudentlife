9f783001281899f9b70d280f5a2da5b75a72cd24
theworldbright <sushionigiris@gmail.com>
2015-03-16 15:45:00 -0700

    Make album a foreign key of photo
    Reformat mainsite.models.py

--- mainsite/models.py
+++ mainsite/models.py
@@ -11,25 +11,31 @@ from workflow.models import Profile, Assignment, WArticle, Revision
 
 class Section(models.Model):
     name = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
+
     def get_absolute_url(self):
-        return reverse('section',kwargs={'section_name':self.name})
+        return reverse('section', kwargs={'section_name': self.name})
 
 class Subsection(models.Model):
     name = models.CharField(max_length=50)
+
     def __str__(self):
         return self.name
+
     def get_absolute_url(self):
-        return reverse('section',kwargs={'section_name':self.name})
+        return reverse('section', kwargs={'section_name': self.name})
 
 class Issue(models.Model):
     name = models.CharField(max_length=200)
     created_date = models.DateTimeField(default=timezone.now)
+
     def __str__(self):
         return self.name
+
     def get_absolute_url(self):
-        return reverse_lazy('issue',kwargs={'issue_id':self.id})
+        return reverse_lazy('issue', kwargs={'issue_id': self.id})
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
@@ -40,14 +46,18 @@ class Article(models.Model):
     subsections = models.ManyToManyField(Subsection, null=True)
     published_date = models.DateTimeField(default=timezone.now)
     updated_date = models.DateTimeField(default=timezone.now)
+
     def __str__(self):
         return self.title
+
     def has_photo(self):
         return self.albums.photos is not None
+
     def slug(self):
         return slugify(self.title)
+
     def get_absolute_url(self):
-        return reverse('article',kwargs={'article_id':self.id,'section_name':self.section.name})
+        return reverse('article', kwargs={'article_id': self.id, 'section_name': self.section.name})
 
 class FrontArticle(models.Model):
     article = models.OneToOneField(Article)
@@ -61,20 +71,21 @@ class CarouselArticle(models.Model):
     def __str__(self):
         return self.article.title
 
+class Album(models.Model):
+    article = models.OneToOneField(Article)
+
+    def __str__(self):
+        return self.article.title
+    
 class Photo(models.Model):
     date = models.DateTimeField(default=timezone.now)
     image = models.ImageField(upload_to='photo/')
     caption = models.TextField(max_length=500, blank=True)
     credit = models.ForeignKey(Profile)
+    album = models.ForgeinKey(Album, null=True)
 
     def __str__(self):
         return self.image.url
-
-class Album(models.Model):
-    article = models.OneToOneField(Article)
-    photos = models.ManyToManyField(Photo,null=True)
-    def __str__(self):
-        return self.article.title
 
 # The assignment form is currently in the mainsite
 # because of a circular dependency on Section.
@@ -82,7 +93,7 @@ class Album(models.Model):
 class AssignmentForm(ModelForm):
     class Meta:
         model = Assignment
-        fields = ['title','content','section','type','receiver','due_date']
+        fields = ['title', 'content', 'section', 'type', 'receiver', 'due_date']
         widgets = {
             'due_date': TextInput(attrs={
                 'type': 'date',
