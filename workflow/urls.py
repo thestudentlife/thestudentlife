41a46b1b32dfc9b773e305f3fd3bba9312691576
Ziqi Xiong <zx012013@mymail.pomona.edu>
2015-03-18 16:47:50 -0700

    Enable text version controll

--- workflow/urls.py
+++ workflow/urls.py
@@ -33,6 +33,7 @@ urlpatterns = patterns('',
     url(r'^articles/issue/(?P<issue_id>[0-9]+)/(?P<pk>[0-9]+)/delete/$',
         group_required('silver')(ArticleDeleteView.as_view()), name='delete_article'),
     url(r'^articles/xml/(?P<article_id>\d+)',views.article_xml,name="article_xml"),
+    url(r'^articles/revision/(?P<pk>\d+)/$',views.revision,name="revision"),
 
     #photos
     url(r'^photos/$',views.photos,name="photos"),
