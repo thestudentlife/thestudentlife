120c693b44ad088820da53b781585f83a5f2f27f
Ziqi Xiong <zx012013@mymail.pomona.edu>
2015-03-17 20:56:31 -0700

    Add carousel view

--- startshell.py
+++
@@ -1,4 +0,0 @@
-import django
-django.setup()
-from mainsite.models import Section, Subsection, Issue, Photo, Article, Album, FrontArticle, CarouselArticle
-from workflow.models import Profile, WArticle, Revision, Review, Assignment
--- workflow/views.py
+++ workflow/views.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-from mainsite.models import Issue, Article, Section, Profile, AssignmentForm, Photo, FrontArticle
+from mainsite.models import Issue, Article, Section, Profile, AssignmentForm, Photo, FrontArticle, CarouselArticle
 from workflow.models import Assignment, RegisterForm, LoginForm, WArticle, Revision
 from django.views.generic.edit import CreateView, DeleteView,UpdateView
 from django.views.generic.list import ListView
@@ -150,6 +150,24 @@ def front(request):
             front = FrontArticle(article=article)
             front.save()
         return redirect(reverse('front'))
+
+@group_required('silver')
+def carousel(request):
+    if request.method=="GET":
+        latest_articles = Article.objects.order_by('-published_date')[:40]
+        latest_articles = list(latest_articles)
+        carousels = CarouselArticle.objects.all()
+        for carousel in carousels:
+            if carousel.article in latest_articles:
+                latest_articles.remove(carousel.article)
+        return render(request, 'front.html',{'articles':latest_articles,'fronts':carousels})
+    else:
+        CarouselArticle.objects.all().delete()
+        for id in request.POST.getlist("selected[]"):
+            article = Article.objects.get(id=id)
+            carousel = FrontArticle(article=article)
+            carousel.save()
+        return redirect(reverse('carousel'))
 
 @group_required('silver')
 def article_xml(request, article_id):
