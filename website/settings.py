62a107e258707d636ee7142411caf78b7d0206ff
theworldbright <sushionigiris@gmail.com>
2015-03-18 17:13:50 -0700

    Add minimal selenium tests
    Refactor test files

--- website/settings.py
+++ website/settings.py
@@ -89,3 +89,25 @@ ENV_PATH = os.path.abspath(os.path.dirname(__file__))
 MEDIA_ROOT = os.path.join(ENV_PATH, 'media/')
 
 LOGIN_URL = '/workflow/login'
+
+from django.conf import settings
+# Specify the selenium test runner
+SELENIUM_TEST_RUNNER = getattr(settings, 'SELENIUM_TEST_RUNNER',
+                             'django_selenium.selenium_runner.SeleniumTestRunner')
+
+SELENIUM_TIMEOUT = getattr(settings, 'SELENIUM_TIMEOUT', 120)
+SELENIUM_DRIVER_TIMEOUT = getattr(settings, 'SELENIUM_DRIVER_TIMEOUT', 10)
+# Specify max waiting time for server to finish processing request and deactivates
+SELENIUM_TEST_SERVER_TIMEOUT = getattr(settings, 'SELENIUM_TEST_SERVER_TIMEOUT', 300)
+
+#------------------ LOCAL ----------------------------------
+SELENIUM_TESTSERVER_HOST = getattr(settings, 'SELENIUM_TESTSERVER_HOST', 'localhost')
+SELENIUM_TESTSERVER_PORT = getattr(settings, 'SELENIUM_TESTSERVER_PORT', 8011)
+SELENIUM_HOST = getattr(settings, 'SELENIUM_HOST', None)
+SELENIUM_PORT = getattr(settings, 'SELENIUM_PORT', 4444)
+
+SELENIUM_DISPLAY = getattr(settings, 'SELENIUM_DISPLAY', ':0')
+# Set the drivers that you want to run your tests against
+SELENIUM_DRIVER = getattr(settings, 'SELENIUM_DRIVER', 'Firefox')
+SELENIUM_DRIVER_OPTS = getattr(settings, 'SELENIUM_DRIVER_OPTS', dict())
+#------------------------------------------------------------
