afeb1530d30974651d00018fc7d56230d07aaaac
theworldbright <sushionigiris@gmail.com>
2015-04-17 22:58:25 -0700

    Improve section navigation bar in issue.html

--- workflow/static/issue_tab.js
+++ workflow/static/issue_tab.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
     $(".articles").fadeOut(0);
-    $("#News-articles").fadeIn();
+    $(".articles:first").fadeIn();
+    $("#sectionNavigation li:first").addClass("active");
 });
 
 $('.section-tab').click(function() {
     var sectionName = $(this).attr('id');
     var idName = "#" + sectionName + "-articles";
+    $('.section-tab').removeClass("active");
+    $(':contains(' + sectionName + ')').addClass("active");
     $(".articles").not(idName).fadeOut(0, function() {
         $(idName).fadeIn();
     });
