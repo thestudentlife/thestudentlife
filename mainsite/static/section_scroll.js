effa0c3b0b48827d5fba2c61d4e01692755ded15
Ziqi Xiong <zx012013@mymail.pomona.edu>
2015-05-07 22:07:35 -0700

    Enable infinite scroll for section

--- mainsite/static/section_scroll.js
+++ mainsite/static/section_scroll.js
@@ -3,7 +3,7 @@ var count = 11;
 var current_url = window.location.href;
 var loading = false;
 
-$(window),scroll(function(){
+$(window).scroll(function(){
     if($(window).scrollTop()+window.innerHeight > $(document).height()-100){
         if(!loading){
             loading = true;
@@ -13,7 +13,10 @@ $(window),scroll(function(){
                     console.log('ajax success');
                     loading = false;
                     count = count + 10;
-                    process(articles);
+                    for(a in articles) {
+                        article = articles[a];
+                        $(process(article)).hide().appendTo('.article_ls').show('slow');
+                    }
                 },
                 error: function(xhr,message,exception){
                     console.log(exception);
@@ -24,5 +27,23 @@ $(window),scroll(function(){
     }
 })
 
+String.prototype.supplant = function (o) {
+    return this.replace(/{([^{}]*)}/g,
+        function (a, b) {
+            var r = o[b];
+            return typeof r === 'string' || typeof r === 'number' ? r : a;
+        }
+    );
+};
 
+function process(article){
+        return "<article>" +
+            "<hgroup>" +
+            "<h1 class='article_title'><a href='{url}'>{title}</a>".supplant({url:article['url'],title:article['title']})+"</h1>" +
+            "<h2 class='article_category'><a href='{url}'>{section}</a>".supplant({url:article['section']['url'],section:article['section']['name']})+"</h2>" +
+            "<h2 class='article_updated_date'>"+article['published_date']+"</h2>" +
+            "</hgroup>" +
+            "<p class='article_content'>{content}</p>".supplant({content:article['content']})+"" +
+            "</article>";
+}
 
