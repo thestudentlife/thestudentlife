c69b71e1d6262317e0180afa26ce1d2825e9a7f8
theworldbright <sushionigiris@gmail.com>
2015-05-23 22:31:38 +0900

    Move every non-section nav item to other

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from mainsite.models import Section, Article, Profile,FrontArticle, CarouselArticle, Copy
+from mainsite.models import Section, Article, Profile, FrontArticle, CarouselArticle, Copy
 import json
 
 def home(request):
     sections = Section.objects.all()
     features = CarouselArticle.objects.all()
     fronts = FrontArticle.objects.all()
-    return render(request, 'index.html', {'sections':sections,'features':features,'fronts':fronts,'recents':get_recent(5),
-                                          'populars':get_popular(5)})
+    return render(request, 'index.html',
+                  {'sections': sections, 'features': features, 'fronts': fronts, 'recents': get_recent(5),
+                   'populars': get_popular(5)})
 
 def section(request, section_slug):
     sections = Section.objects.all()
@@ -18,15 +19,15 @@ def section(request, section_slug):
             this_section = section;
     if request.is_ajax():
         count = int(request.GET['count'])
-        articles = this_section.articles.order_by('-published_date')[count:count+10]
+        articles = this_section.articles.order_by('-published_date')[count:count + 10]
         articles_in_json = []
         for article in articles:
             if article.published:
                 articles_in_json.append(article_ajax_object(article))
-        return HttpResponse(json.dumps(articles_in_json),content_type='application/json')
+        return HttpResponse(json.dumps(articles_in_json), content_type='application/json')
     articles = this_section.articles.filter(published=True).order_by('-published_date')[:10]
     return render(request, 'section.html', {"section": this_section, "sections": sections, "articles": articles,
-                                            'recents': get_recent(5),'populars':get_popular(5)});
+                                            'recents': get_recent(5), 'populars': get_popular(5)});
 
 def article(request, section_name, article_id, article_name='default'):
     sections = Section.objects.all()
@@ -34,7 +35,7 @@ def article(request, section_name, article_id, article_name='default'):
     article.click()
     article.save()
     return render(request, 'article.html', {"sections": sections, "article": article, 'recents': get_recent(5),
-                                            'populars':get_popular(5)})
+                                            'populars': get_popular(5)})
 
 def person(request, person_id, person_name='ZQ'):
     sections = Section.objects.all()
@@ -48,30 +49,29 @@ def person(request, person_id, person_name='ZQ'):
         return render(request, 'photographer.html', {"sections": sections, "photographs": photographs})
     return HttpResponse('His/Her profile is not public.')
 
-def about(request, info):
+def other(request, info):
     sections = Section.objects.all()
-    template = "about/" + info+".html";
-    return render(request, "about/about.html", {"sections": sections, 'info': template})
+    template = "other/" + info + ".html";
+    return render(request, "other/about.html", {"sections": sections, 'info': template})
 
 def archives(request):
+    sections = Section.objects.all()
     copies = Copy.objects.all()
-    return render(request,'archives.html',{'copies':copies})
+    return render(request, 'other/archives.html', {"sections": sections, 'copies': copies})  # Create json objects for an article
 
-
-# Create json objects for an article
 def article_ajax_object(article):
     fmt = '%b. %d, %Y, %I:%M %p'
     obj = dict()
     obj['url'] = article.get_absolute_url()
     obj['title'] = article.title
-    obj['section'] = {'name':article.section.name,
-                      'url':article.section.get_absolute_url()}
+    obj['section'] = {'name': article.section.name,
+                      'url': article.section.get_absolute_url()}
     obj['published_date'] = article.published_date.strftime(fmt)
-    obj['content'] = article.content[0:100]+'...'
-    obj['authors']=[]
+    obj['content'] = article.content[0:100] + '...'
+    obj['authors'] = []
     for author in article.authors.all():
-        obj['authors'].append({'name':author.display_name,
-                              'url':author.get_absolute_url()})
+        obj['authors'].append({'name': author.display_name,
+                               'url': author.get_absolute_url()})
     return obj
 
 def get_recent(n):
