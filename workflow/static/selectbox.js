987ceb445f2cb0aa4fcbb3ce319ebc4736c4fbaa
ZiqiXiong <zx012013@mymail.pomona.edu>
2015-10-25 19:25:05 -0700

    Enable ordering front and carousel articles manually

--- workflow/static/selectbox.js
+++ workflow/static/selectbox.js
@@ -1,3 +1,10 @@
+function swapNodes(a, b) {
+    var aparent = a.parentNode;
+    var asibling = a.nextSibling === b ? a : a.nextSibling;
+    b.parentNode.insertBefore(a, b);
+    aparent.insertBefore(b, asibling);
+}
+
 $("#btnLeft").click(function () {
     var selectedItem = $("#rightValues option:selected");
     $("#leftValues").append(selectedItem);
@@ -7,6 +14,18 @@ $("#btnRight").click(function () {
     var selectedItem = $("#leftValues option:selected");
     $("#rightValues").append(selectedItem);
 });
+
+$("#btnUp").click(function(){
+    var selectedItem = $("#rightValues option:selected");
+    var previousItem = $(selectedItem[0]).prev();
+    swapNodes(selectedItem[0],previousItem[0]);
+})
+
+$("#btnDown").click(function(){
+    var selectedItem = $("#rightValues option:selected");
+    var NextItem = $(selectedItem[0]).next();
+    swapNodes(selectedItem[0],NextItem[0]);
+})
 
 $("#rightValues").change(function () {
     var selectedItem = $("#rightValues option:selected");
@@ -25,6 +44,19 @@ $("#btnRight2").click(function () {
 $("#rightValues2").change(function () {
     var selectedItem = $("#rightValues2 option:selected");
 });
+
+$("#btnUp2").click(function(){
+    var selectedItem = $("#rightValues2 option:selected");
+    var previousItem = $(selectedItem[0]).prev();
+    swapNodes(selectedItem[0],previousItem[0]);
+})
+
+$("#btnDown2").click(function(){
+    var selectedItem = $("#rightValues2 option:selected");
+    var NextItem = $(selectedItem[0]).next();
+    swapNodes(selectedItem[0],NextItem[0]);
+})
+
 
 $('#submit_button').click(function(){
     $('#rightValues option').prop("selected",true);
