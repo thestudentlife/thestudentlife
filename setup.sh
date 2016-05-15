80d99c6f99fce374a5149e000a7d807d8547cdee
ZiqiXiong <zx012013@mymail.pomona.edu>
2016-05-14 18:28:55 -0700

    Add loading gif to home page

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 cwd=$(pwd)
-echo $(pip3 --version)
-pip3 install virtualenv
+echo $(pip3.4 --version)
+pip3.4 install virtualenv
 if [ ! -d "$cwd/ve" ]; then
-    virtualenv -p $(which python3) -q $cwd/ve
+    virtualenv -p $(which python3.4) -q $cwd/ve
     echo "Virtualenv created."
 fi
 
 source $cwd/ve/bin/activate
 
 if [ ! -f "$cwd/ve/updated" -o $cwd/requirements.txt -nt $cwd/ve/updated ]; then
-    pip3 install -r $cwd/requirements.txt
+    pip3.4 install -r $cwd/requirements.txt
     touch $cwd/ve/updated
     echo "Requirements for TSL installed"
 fi
@@ -26,7 +26,7 @@ rm -rf mainsite/migrations/*;
 cd mainsite && mkdir migrations;
 touch mainsite/migrations/__init__.py;
 cd ..;
-python3 manage.py makemigrations;
+python3.4 manage.py makemigrations;
 mkdir website/media/;
 mkdir website/media/photo/;
 mkdir website/media/thumbs/;
@@ -38,7 +38,7 @@ cp website/fixtures/ramune.jpg website/media/photo/ramune.jpg;
 cp website/fixtures/goldengatebridge_thumbnail.jpg website/media/thumbs/goldengatebridge_thumbnail.jpg;
 cp website/fixtures/pearlharbor_thumbnail.jpg website/media/thumbs/pearlharbor_thumbnail.jpg;
 cp website/fixtures/ramune_thumbnail.jpg website/media/thumbs/ramune_thumbnail.jpg;
-python3 manage.py migrate;
-python3 manage.py loaddata website/fixtures/initial_data.json;
-python3 manage.py collectstatic --noinput;
-python3 manage.py runserver;
+python3.4 manage.py migrate;
+python3.4 manage.py loaddata website/fixtures/initial_data.json;
+python3.4 manage.py collectstatic --noinput;
+python3.4 manage.py runserver;
