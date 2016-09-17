c33215019e7f02b2c2e00ce208f419e8f2abff80
Kent Shikama <kent@kentshikama.com>
2016-09-16 19:21:24 -0700

    Fix article image resizing issue

--- mainsite/static/resize_image.js
+++ mainsite/static/resize_image.js
@@ -1,17 +1,14 @@
-
-
-function resize(){
-    var maxWidth =$('.article_content').width();
-    $('.article_content img').each(function(){
-    var $img = $(this);
-    var imgRatio = $img.width()/$img.height();
-    $img.width(maxWidth*3/4);
-    $img.height($img.width()/imgRatio);
+function resize() {
+    var maxWidth = $('.article_content > p').width();
+    $('.article_content img').each(function () {
+        var $img = $(this);
+        var imgRatio = $img.width() / $img.height();
+        $img.width(maxWidth);
+        $img.height($img.width() / imgRatio);
     });
 }
 
 new ResizeSensor(jQuery('.article_content'), resize);
-
 resize();
 
 
