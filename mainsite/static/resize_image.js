eb45c5556ad90b8f7dcde24bde9d17640af37b61
Kent Shikama <kent@kentshikama.com>
2016-09-17 17:04:45 -0700

    Adjust height-long photos so they are 70% of p

--- mainsite/static/resize_image.js
+++ mainsite/static/resize_image.js
@@ -3,13 +3,16 @@ function resize() {
     $('.article_content img').each(function () {
         var $img = $(this);
         var imgRatio = $img.width() / $img.height();
-        $img.width(maxWidth);
-        $img.height($img.width() / imgRatio);
+        if (imgRatio > 1) {
+            $img.width(maxWidth);
+            $img.height($img.width() / imgRatio);
+        } else {
+            $img.width(0.7 * maxWidth);
+            $img.height($img.width() / imgRatio);
+        }
     });
 }
 
 new ResizeSensor(jQuery('.article_content'), resize);
 resize();
-
-
 
