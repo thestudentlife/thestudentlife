6681b15164b34617637398f8d806d19d87c14fa6
ZiqiXiong <zx012013@mymail.pomona.edu>
2017-03-02 10:02:36 -0800

    Add article model to admin site

--- mainsite/admin.py
+++ mainsite/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from mainsite.models import Section, Issue
+from mainsite.models import Section, Issue, Article
 
 admin.site.register(Section);
 admin.site.register(Issue);
+admin.site.register(Article);
